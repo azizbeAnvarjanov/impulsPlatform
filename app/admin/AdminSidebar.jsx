2acdc00dd997e0a1a239c578e1274fd1ddfb0e63
azizbeAnvarjanov <azizbeanvarjanov@gmail.com>
2024-12-24 11:52:43 +0500

    change version react-day-picker

--- app/admin/AdminSidebar.jsx
+++ app/admin/AdminSidebar.jsx
@@ -22,7 +22,12 @@ const AdminSidebar = () => {
   return (
     <div>
       <div className="border border-r-2 fixed left-0 top-0 h-screen w-[35vh] p-5">
-        <Link href="/admin" className="p-3 hover:bg-muted rounded-md w-full flex font-bold">Main page</Link>
+        <Link
+          href="/admin"
+          className="p-3 hover:bg-muted rounded-md w-full flex font-bold"
+        >
+          Main page
+        </Link>
         <ul className="mt-4">
           <li className="p-3 hover:bg-muted rounded-md">
             <Link href="/admin/departments">
@@ -44,7 +49,14 @@ const AdminSidebar = () => {
               <h1 className="font-bold">Barcha jihozlar</h1>
             </Link>
           </li>
-          <Button className="w-[90%] left-[50%] absolute bottom-5 -translate-x-[50%]" variant="destructive" onClick={handleLogout}>Logout</Button> <br />
+          <Button
+            className="w-[90%] left-[50%] absolute bottom-5 -translate-x-[50%]"
+            variant="destructive"
+            onClick={handleLogout}
+          >
+            Logout
+          </Button>{" "}
+          <br />
           {/* {user ? <>{user.uid}</>:<>n</>} */}
         </ul>
       </div>
